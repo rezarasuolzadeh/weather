8fb5da3b6780951032191bb3bd8095f4e4796efb
RezaRasoulzadeh <reza.rassoulzadeh@gmail.com>
2020-09-22 10:28:02 +0330

    add icon of the app and some dependencies.

---
+++ app/src/main/java/ir/rezarasuolzadeh/weather/view/activities/WeatherActivity.kt
@@ -0,0 +1,15 @@
+package ir.rezarasuolzadeh.weather.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ir.rezarasuolzadeh.weather.R
+
+class WeatherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+
+
+
+    }
+}
